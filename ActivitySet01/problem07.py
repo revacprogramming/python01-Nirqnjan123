88b1e89e346ad5b5e0b8ac39259ead00fd31c2e8
Niranjan123 <96220221+Nirqnjan123@users.noreply.github.com>
2022-06-08 09:37:55 +0000

    assignment 1

--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -1,3 +1,7 @@
 # Strings
 
-text = "X-DSPAM-Confidence:    0.8475"
+#text = "X-DSPAM-Confidence:    0.8475"
+text = "X-DSPAM-Confidence: 0.8475" 
+find = text. find ('0.8475') 
+a = float(find) 
+print(text[find:])
--- ActivitySet01/problem14.py
+++ ActivitySet01/problem14.py
@@ -1,2 +1,11 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+a = int(input("enter a number"))
+b = int(input("enter a number"))
+c = int(input("enter a number"))
+if a>b and a>c:
+  print(f"{a} is the largest numbers")
+elif b>a and b>c:
+  print(f"{b} is the largest numbers")
+else:
+  print(f"{a} is the largest numbers")
